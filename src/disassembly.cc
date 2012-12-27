16668fc54f1db5e92b4815a62b8e15433cf4ebdb
Simon Kagstrom <simon.kagstrom@netinsight.net>
2012-12-27 14:23:27 +0100

    disassembly: Pick up instruction encoding etc from opdis

--- src/disassembly.cc
+++ src/disassembly.cc
@@ -8,12 +8,40 @@
 
 using namespace emilpro;
 
+class Operand : public IOperand
+{
+public:
+	Operand(const char *encoding) :
+		m_encoding(encoding)
+	{
+	}
+
+	virtual std::string &getEncoding()
+	{
+		return m_encoding;
+	}
+
+private:
+	std::string m_encoding;
+};
+
 class Instruction : public IInstruction
 {
 public:
-	Instruction(uint64_t address) :
-		m_address(address)
+	Instruction(uint64_t address, InstructionType_t type, const char *encoding, Ternary_t privileged) :
+		m_address(address),
+		m_type(type),
+		m_encoding(encoding),
+		m_privileged(privileged)
 	{
+	}
+
+	virtual ~Instruction()
+	{
+		for (OperandList_t::iterator it = m_operands.begin();
+				it != m_operands.end();
+				it++)
+			delete *it;
 	}
 
 	uint64_t getAddress()
@@ -21,8 +49,33 @@ public:
 		return m_address;
 	}
 
+	Ternary_t isPrivileged()
+	{
+		return m_privileged;
+	}
+
+	InstructionType_t getType()
+	{
+		return m_type;
+	}
+
+	std::string &getEncoding()
+	{
+		return m_encoding;
+	}
+
+	const OperandList_t &getOperands()
+	{
+		return m_operands;
+	}
+
 private:
 	uint64_t m_address;
+	InstructionType_t m_type;
+	std::string m_encoding;
+	Ternary_t m_privileged;
+
+	IInstruction::OperandList_t m_operands;
 };
 
 class Disassembly : public IDisassembly
@@ -70,12 +123,44 @@ public:
 private:
 	void opdisDisplay(const opdis_insn_t *insn)
 	{
-	    panic_if(!m_list,
-	             "No list when displaying!");
+		panic_if(!m_list,
+				"No list when displaying!");
 
-	    Instruction *cur = new Instruction(m_startAddress + insn->offset);
+		uint64_t address = m_startAddress + insn->offset;
+		IInstruction::InstructionType_t type = IInstruction::IT_UNKNOWN;
+		const char *encoding = insn->ascii;
+		IInstruction::Ternary_t privileged = IInstruction::T_unknown;
 
-	    m_list->push_back(cur);
+		if (insn->status & opdis_decode_mnem_flags) {
+			privileged = IInstruction::T_false;
+
+			switch (insn->category)
+			{
+			case opdis_insn_cat_cflow:
+				type = IInstruction::IT_CFLOW;
+				break;
+			case opdis_insn_cat_lost:
+			case opdis_insn_cat_stack:
+				type = IInstruction::IT_DATA_HANDLING;
+				break;
+			case opdis_insn_cat_test:
+			case opdis_insn_cat_math:
+			case opdis_insn_cat_bit:
+				type = IInstruction::IT_ARITHMETIC_LOGIC;
+				break;
+			case opdis_insn_cat_priv:
+				type = IInstruction::IT_OTHER;
+				privileged = IInstruction::T_true;
+				break;
+			default:
+				type = IInstruction::IT_OTHER;
+				break;
+			}
+		}
+
+		Instruction *cur = new Instruction(address, type, encoding, privileged);
+
+		m_list->push_back(cur);
 	}
 
 	static void opdisDisplayStatic(const opdis_insn_t *insn, void *arg)
--- src/include/iinstruction.hh
+++ src/include/iinstruction.hh
@@ -1,16 +1,55 @@
 #pragma once
 
+#include <stdint.h>
 #include <string>
+#include <list>
 
 namespace emilpro
 {
+	class IOperand
+	{
+	public:
+		virtual ~IOperand()
+		{
+		}
+
+		virtual std::string &getEncoding() = 0;
+	};
+
 	class IInstruction
 	{
 	public:
+		typedef enum
+		{
+			IT_UNKNOWN,
+			IT_CFLOW,
+			IT_DATA_HANDLING,
+			IT_ARITHMETIC_LOGIC,
+			IT_OTHER,
+		} InstructionType_t;
+
+		typedef enum
+		{
+			T_false = false,
+			T_true = true,
+			T_unknown,
+		} Ternary_t;
+
+		typedef std::list<const IOperand *> OperandList_t;
+
+
 		virtual ~IInstruction()
 		{
 		}
 
 		virtual uint64_t getAddress() = 0;
+
+		virtual Ternary_t isPrivileged() = 0;
+
+		virtual InstructionType_t getType() = 0;
+
+		virtual std::string &getEncoding() = 0;
+
+		virtual const OperandList_t &getOperands() = 0;
 	};
 }
