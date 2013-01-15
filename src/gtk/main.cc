63f7434718edfd081142bcdc1c8a48a6e87653e6
Simon Kagstrom <simon.kagstrom@gmail.com>
2013-01-15 14:48:33 +0100

    gtk: Add file-open dialogue

--- src/gtk/main.cc
+++ src/gtk/main.cc
@@ -1,6 +1,7 @@
 #include <gtkmm.h>
 
 #include <model.hh>
+#include <utils.hh>
 
 using namespace emilpro;
 
@@ -13,17 +14,53 @@ public:
 
 	void init(int argc, char **argv)
 	{
-		Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+		m_app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
-		Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("/home/ska/projects/emilpro/src/gtk/emilpro.glade");
+		m_builder = Gtk::Builder::create_from_file("/home/ska/projects/emilpro/src/gtk/emilpro.glade");
 
+		Gtk::ImageMenuItem *fileOpenItem;
+		m_builder->get_widget("file_menu_open", fileOpenItem);
+
+		panic_if (!fileOpenItem,
+				"Can't get file_menu_open");
+		fileOpenItem->signal_activate().connect(sigc::mem_fun(*this, &EmilProGui::onFileOpen));
+	}
+
+	void run()
+	{
 		Gtk::Window * mainWindow = NULL;
-		builder->get_widget("main window", mainWindow);
+		m_builder->get_widget("main_window", mainWindow);
 
-		app->run(*mainWindow);
+		m_app->run(*mainWindow);
 	}
 
 protected:
+	void onFileOpen()
+	{
+		Gtk::FileChooserDialog *openFile = NULL;
+		m_builder->get_widget("file_chooser", openFile);
+
+		panic_if (!openFile,
+				"Open file dialogue does not exist");
+
+		int v = openFile->run();
+
+		if (v == Gtk::RESPONSE_ACCEPT) {
+			size_t sz;
+
+			void *data = read_file(&sz, "%s", openFile->get_filename().c_str());
+
+			if (data) {
+				Model::instance().addData(data, sz);
+			}
+		}
+
+		openFile->hide();
+	}
+
+private:
+	Glib::RefPtr<Gtk::Application> m_app;
+	Glib::RefPtr<Gtk::Builder> m_builder;
 };
 
 int main(int argc, char **argv)
@@ -31,6 +68,8 @@ int main(int argc, char **argv)
 	EmilProGui gui;
 
 	gui.init(argc, argv);
+
+	gui.run();
 
 	return 0;
 }
