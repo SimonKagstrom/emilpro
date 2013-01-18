4fe3c59abf77da03fd3ea11e7cb83277bfbcc3a5
Simon Kagstrom <simon.kagstrom@gmail.com>
2013-01-18 09:47:34 +0100

    gui: Populate symbol list on file open
    The GUI breaks from this, but at least it's populated.

--- src/gtk/main.cc
+++ src/gtk/main.cc
@@ -3,7 +3,25 @@
 #include <model.hh>
 #include <utils.hh>
 
+#include <string>
+
 using namespace emilpro;
+
+class SymbolModelColumns : public Gtk::TreeModelColumnRecord
+{
+public:
+	SymbolModelColumns()
+	{
+		add(m_pixbuf);
+		add(m_address);
+		add(m_name);
+	}
+
+	Gtk::TreeModelColumn<Glib::RefPtr<Gdk::Pixbuf>> m_pixbuf;
+	Gtk::TreeModelColumn<uint64_t> m_address;
+	Gtk::TreeModelColumn<Glib::ustring> m_name;
+};
+
 
 class EmilProGui
 {
@@ -24,6 +42,11 @@ public:
 		panic_if (!fileOpenItem,
 				"Can't get file_menu_open");
 		fileOpenItem->signal_activate().connect(sigc::mem_fun(*this, &EmilProGui::onFileOpen));
+
+		m_symbolListStore = Glib::RefPtr<Gtk::ListStore>::cast_static(m_builder->get_object("symbol_liststore"));
+		panic_if (!m_symbolListStore,
+				"Can't get symbol liststore");
+
 	}
 
 	void run()
@@ -59,11 +82,29 @@ protected:
 
 		if (!Model::instance().addData(data, sz))
 			return;
+
+		SymbolModelColumns columns;
+
+		const Model::SymbolList_t &syms = Model::instance().getSymbols();
+
+		for (Model::SymbolList_t::const_iterator it = syms.begin();
+				it != syms.end();
+				++it) {
+			ISymbol *cur = *it;
+
+			Gtk::ListStore::iterator row = m_symbolListStore->append();
+
+			(*row)[columns.m_address] = cur->getAddress();
+			(*row)[columns.m_name] = cur->getName();
+		}
 	}
 
 private:
+	 typedef Gtk::TreeModel::Children TreeModelChildren_t;
+
 	Gtk::Main *m_app;
 	Glib::RefPtr<Gtk::Builder> m_builder;
+	Glib::RefPtr<Gtk::ListStore> m_symbolListStore;
 };
 
 int main(int argc, char **argv)
