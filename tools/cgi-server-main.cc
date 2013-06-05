2283d8501b59a8825bddaced03b8a25bfff7d43e
Simon Kagstrom <simon.kagstrom@gmail.com>
2013-06-05 12:38:23 +0200

    tools: cgi-server: Daemonize and accept quit command if allowed

--- tools/cgi-server-main.cc
+++ tools/cgi-server-main.cc
@@ -7,30 +7,71 @@
 #include <string>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 using namespace emilpro;
 
 void usage()
 {
-	printf("cgi-server <configuration-dir> <in-fifo> <out-fifo>\n");
+	printf(
+			"cgi-server <configuration-dir> <in-fifo> <out-fifo> [-q] [-f]\n"
+			"   -q  Accept quit command\n"
+			"   -f  Run in foreground\n"
+			);
 	exit(1);
 }
 
 int main(int argc, const char *argv[])
 {
-	if (argc != 4)
+	if (argc < 4)
 		usage();
 
+	bool honorQuit = false;
+	bool foreground = false;
 	std::string baseDirectory = argv[1];
 	const char *inFifoName = argv[2];
 	const char *outFifoName = argv[3];
 
-	mkfifo(inFifoName, S_IRUSR | S_IWUSR);
-	mkfifo(outFifoName, S_IRUSR | S_IWUSR);
+	if (argc > 4 && strcmp(argv[4], "-q") == 0)
+		honorQuit = true;
+	if (argc > 5 && strcmp(argv[5], "-f") == 0)
+		foreground = true;
 
 	Configuration::setBaseDirectory(baseDirectory);
 	InstructionFactory::instance();
 	CgiServer server;
+
+	if (!foreground) {
+		pid_t child;
+
+		child = fork();
+
+		if (child < 0) {
+			perror("Fork failed?\n");
+			exit(3);
+		} else if (child == 0) {
+			child = fork();
+
+			if (child < 0) {
+				perror("Fork failed?\n");
+				exit(3);
+			} else if (child > 0) {
+				// Second parent
+				exit(0);
+			} else {
+			    freopen( "/dev/null", "r", stdin);
+			    freopen( "/dev/null", "w", stdout);
+			    freopen( "/dev/null", "w", stderr);
+			}
+		} else {
+			// First parent
+			exit(0);
+		}
+	}
+
+	mkdir(baseDirectory.c_str(), 0755);
+	mkfifo(inFifoName, S_IRUSR | S_IWUSR);
+	mkfifo(outFifoName, S_IRUSR | S_IWUSR);
 
 	while (1)
 	{
@@ -43,6 +84,9 @@ int main(int argc, const char *argv[])
 
 		std::string cur(data);
 		free(data);
+
+		if (honorQuit && cur.substr(0, 4) == "quit")
+			return 2;
 
 		server.request(cur);
 		std::string reply = server.reply();
