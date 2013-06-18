162fe93c93ab5f0af164cddd38af1319deaa61c4
Simon Kagstrom <simon.kagstrom@gmail.com>
2013-06-18 13:02:11 +0200

    robot-framework: Add data setup helper script
    Removes the need of the data directory

---
+++ tools/setup-conf-dir.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+if [ $# -lt 2 ] ; then
+    echo "Usage: setup-conf-dir.sh <out-directory> <remote/local> <timestamp> [insn-name timestamp]"
+    exit 1
+fi
+
+DIR=$1
+TYPE=$2
+TS=$3
+
+install -d $DIR
+install -d $DIR/remote
+install -d $DIR/local
+install -d $DIR/configuration
+
+shift; shift; shift
+
+while [ $# -gt 1 ] ; do
+    NAME=$1
+    INSN_TS=$2
+
+    cat <<EOF > $DIR/$TYPE/${NAME}.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<emilpro>
+  <InstructionModel name="$NAME" architecture="mips" timestamp="$INSN_TS">
+  </InstructionModel>
+</emilpro>
+EOF
+
+    shift ; shift
+done
+
+
+if [ $TS -eq 0 ] ; then
+    exit 0
+fi
+
+cat << EOF > $DIR/remote/serverTimestamp.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<emilpro>
+  <ServerTimestamps>
+     <InstructionModelTimestamp>$TS</InstructionModelTimestamp>
+  </ServerTimestamps>
+</emilpro>
+EOF
