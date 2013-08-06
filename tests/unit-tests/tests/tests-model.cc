155bc64ce224e3b91ccf37f50ec96969fb058395
Simon Kagstrom <simon.kagstrom@gmail.com>
2013-08-06 10:41:37 +0200

    unit test: Move mocked symbol provider to a header file

--- tests/unit-tests/tests/tests-model.cc
+++ tests/unit-tests/tests/tests-model.cc
@@ -11,47 +11,7 @@
 using namespace emilpro;
 
 #include "assembly-dumps.h"
-
-class MockSymbolProvider : public ISymbolProvider
-{
-public:
-	MockSymbolProvider() :
-		m_listener(NULL)
-	{
-		SymbolFactory::instance().registerProvider(this);
-	}
-
-	unsigned match(void *data, size_t dataSize)
-	{
-		return ISymbolProvider::PERFECT_MATCH;
-	}
-
-	bool parse(void *data, size_t dataSize, ISymbolListener *listener)
-	{
-		m_listener = listener;
-
-		return true;
-	}
-
-	void addSymbol(uint64_t start, uint64_t end)
-	{
-		ASSERT_TRUE(m_listener);
-
-		ISymbol &p = SymbolFactory::instance().createSymbol(ISymbol::LINK_NORMAL,
-				ISymbol::SYM_TEXT,
-				fmt("%llx..%llx\n", (long long)start, (long long)end).c_str(),
-				NULL,
-				start,
-				end - start,
-				true,
-				true);
-
-		m_listener->onSymbol(p);
-	}
-
-private:
-	ISymbolListener *m_listener;
-};
+#include "../mock-symbol-provider.hh"
 
 TESTSUITE(model)
 {
