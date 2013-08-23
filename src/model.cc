SimonKagstrom/emilpro

commit 05a5a2f44fcd6c3b1bc9d4581e4b7955e805987e

model: Disassemble executable symbols, not only TEXT ones

src/model.cc
@@ -146,6 +146,10 @@ void Model::fillCacheWithSymbol(ISymbol *sym)
 			++it) {
 		IInstruction *cur = *it;
 
+		// We can have multiple overlapping symbols (typically sections/normal syms)
+		if (m_instructionCache[cur->getAddress()])
+			continue;
+
 		m_instructionCache[cur->getAddress()] = cur;
 
 		if (cur->getBranchTargetAddress() != IInstruction::INVALID_ADDRESS)
@@ -196,7 +200,7 @@ void Model::parseAll()
 		if (curCore == cores)
 			curCore = 0;
 
-		if (cur->getType() != ISymbol::SYM_TEXT)
+		if (!cur->isExecutable())
 			continue;
 
 		m_workQueues[curCore].push_back(cur);

commit 09f74dd4e6dcd3b1d1a5ed7c4b2dd2c18588758d

model: Fix memory leaks
One introduced in

  05a5a2f44fcd6c3b1bc9d4581e4b7955e805987e

since instructions are allocated multiple times, but never deleted.
The other has been there for a long time...

src/model.cc
@@ -60,6 +60,8 @@ Model::~Model()
 	for (unsigned i = 0; i < cores; i++) {
 		if (m_threads[i])
 			m_threads[i]->join();
+
+		delete m_threads[i];
 	}
 
 	delete[] m_threads;
@@ -147,8 +149,10 @@ void Model::fillCacheWithSymbol(ISymbol *sym)
 		IInstruction *cur = *it;
 
 		// We can have multiple overlapping symbols (typically sections/normal syms)
-		if (m_instructionCache[cur->getAddress()])
+		if (m_instructionCache[cur->getAddress()]) {
+			delete cur;
 			continue;
+		}
 
 		m_instructionCache[cur->getAddress()] = cur;
 