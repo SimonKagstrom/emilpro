c935a658244cdc26263a9d67a3e962fdbdf94f6f
Simon Kagstrom <simon.kagstrom@netinsight.net>
2013-10-17 14:52:26 +0200

    build: Download and build binutils

---
+++ cmake/BuildBinutils.cmake
@@ -0,0 +1,18 @@
+add_custom_command (OUTPUT binutils.tar.bz2
+	COMMAND wget -O binutils.tar.bz2 http://ftp.gnu.org/gnu/binutils/binutils-2.23.2.tar.bz2
+)
+
+add_custom_command (OUTPUT binutils/.binutils-built
+	COMMAND rm -rf binutils
+	COMMAND tar -xf binutils.tar.bz2
+	COMMAND mv -f binutils-2.23.2 binutils/
+	COMMAND cd binutils && ./configure --enable-targets=all --disable-ld --disable-gold --prefix=`pwd`/../install-binutils && make && make install
+	COMMAND cp binutils/include/libiberty.h install-binutils/include/
+	COMMAND cp binutils/include/demangle.h install-binutils/include/
+	COMMAND touch binutils/.binutils-built
+	DEPENDS binutils.tar.bz2
+)
+
+add_custom_target(binutils ALL
+	DEPENDS binutils/.binutils-built
+)
