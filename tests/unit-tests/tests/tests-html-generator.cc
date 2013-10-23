700ab66466527beb1f2ec616cb84be1c9d4b76e8
Simon Kagstrom <simon.kagstrom@gmail.com>
2013-10-23 06:18:47 +0200

    unit test: html-generator: Check output ordering

--- tests/unit-tests/tests/tests-html-generator.cc
+++ tests/unit-tests/tests/tests-html-generator.cc
@@ -29,7 +29,7 @@ TESTSUITE(html_generator)
 		gen.addData("23.53.38.151", bfd_arch_sparc);
 		gen.addData("202.248.194.70", bfd_arch_sparc);
 		gen.addData("202.248.194.70", bfd_arch_unknown);
-		gen.addData("", bfd_arch_unknown); // Environment not set
+		gen.addData("", bfd_arch_mips); // Environment not set
 
 
 		mock_write_file(write_callback);
@@ -46,6 +46,21 @@ TESTSUITE(html_generator)
 		ASSERT_TRUE(path_to_data[xmlFilename].find("Unknown") != std::string::npos);
 
 		ASSERT_TRUE(gen.m_countryCount["Sweden"] == 3U);
+
+		// Test that the output HTML is ordered
+		size_t swePos = path_to_data[statsFilename].find("1</b>. Sweden (3)");
+		size_t japanPos = path_to_data[statsFilename].find("2</b>. Japan (2)");
+
+		ASSERT_TRUE(swePos != std::string::npos);
+		ASSERT_TRUE(japanPos != std::string::npos);
+		ASSERT_TRUE(swePos < japanPos);
+
+		size_t i386Pos = path_to_data[statsFilename].find("1</b>. i386 (3)");
+		size_t sparcPos = path_to_data[statsFilename].find("2</b>. sparc (2)");
+
+		ASSERT_TRUE(i386Pos != std::string::npos);
+		ASSERT_TRUE(sparcPos != std::string::npos);
+		ASSERT_TRUE(i386Pos < sparcPos);
 
 		gen.destroy();
 	}
