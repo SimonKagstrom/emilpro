de58f4316877ed4553d9b1331d86fdcd57172f2c
Simon Kagstrom <simon.kagstrom@gmail.com>
2014-01-06 12:48:30 +0100

    server: Make built-in-instructions valid XML

--- tools/built_in_instruction_models.hh
+++ tools/built_in_instruction_models.hh
@@ -2,4 +2,7 @@
 
 #include <string>
 
-std::string built_in_instruction_models_xml;
+std::string built_in_instruction_models_xml =
+"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+"<emilpro>\n"
+"</emilpro>\n";
