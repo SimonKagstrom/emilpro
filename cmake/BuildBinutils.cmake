02523f55e55407cea3f728f490043c8fae1d5699
Alex Chernetz <andy80586@gmail.com>
2015-05-01 12:06:49 -0700

    Get binutils from its official Git repo to pull in the latest fixes

--- cmake/BuildBinutils.cmake
+++ cmake/BuildBinutils.cmake
@@ -1,17 +1,16 @@
-add_custom_command (OUTPUT binutils.tar.bz2
-	COMMAND wget -O binutils.tar.bz2 http://ftp.gnu.org/gnu/binutils/binutils-2.23.2.tar.bz2
+add_custom_command(OUTPUT .binutils-downloaded
+	COMMAND git clone git://sourceware.org/git/binutils-gdb.git binutils-gdb
+	COMMAND touch .binutils-downloaded
 )
 
 add_custom_command (OUTPUT binutils/.binutils-built
 	COMMAND rm -rf binutils
-	COMMAND tar -xf binutils.tar.bz2
-	COMMAND mv -f binutils-2.23.2 binutils/
-	COMMAND cd binutils && patch -p1 < ${BASE_DIR}/external/binutils/binutils-fix-ineffectual-zero-of-cache.patch
+	COMMAND cp -R binutils-gdb binutils
 	COMMAND cd binutils && ./configure --disable-werror --enable-targets=all --disable-ld --disable-gold --prefix=`pwd`/../install-binutils && make && make install
 	COMMAND cp binutils/include/libiberty.h install-binutils/include/
 	COMMAND cp binutils/include/demangle.h install-binutils/include/
 	COMMAND touch binutils/.binutils-built
-	DEPENDS binutils.tar.bz2
+	DEPENDS .binutils-downloaded
 )
 
 add_custom_target(binutils ALL
