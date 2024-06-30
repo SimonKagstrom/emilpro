25b64d265cdaa9e8195adaca9fde00f29799d183
Simon Kagstrom <simon.kagstrom@gmail.com>
2024-06-30 16:01:30 +0200

    main: Usage() if file doesn't exist

--- qt/emilpro/main.cc
+++ qt/emilpro/main.cc
@@ -1,13 +1,34 @@
 #include "mainwindow.hh"
 
 #include <QApplication>
+#include <QFile>
+#include <fmt/format.h>
+#include <stdlib.h>
+
+namespace
+{
+
+void
+Usage(auto app_name)
+{
+    fmt::print("Usage: {} [filename]\n", app_name);
+    exit(1);
+}
+
+} // namespace
 
 int
 main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
 
+    if (argc > 1 && !QFile(argv[1]).exists())
+    {
+        fmt::print("File {} not found\n\n", argv[1]);
+        Usage(argv[0]);
+    }
+
+    MainWindow w;
     w.Init(argc, argv);
 
     w.show();
